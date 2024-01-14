fbbab8e2e751f74b7f59903a65b24e54152391d4
Allyson-Santana <santana.allyson459@gmail.com>
2024-01-14 17:07:05 -0300

    feat: wip login

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react"
+import { Login } from "./types"
+
 export function SignIn() {
+    const [login, setLogin] = useState<Login>({
+        email: '',
+        password: ''
+    });
+
+    const changeLoginState = (event: any) => {
+        const { name, value } = event.target
+        
+        setLogin((previous) => ({...previous, [name]: value}));
+    }
+
+
+    const submitLogin = () => {
+        console.log(login)
+    }
+
     return (
-        <>Sign-In</>
+        <div className="container-sign-in">
+            <input type="text" name="email" id="email" onChange={changeLoginState}/>
+            <input type="text" name="password" id="password" onChange={changeLoginState}/>
+
+            <button type="button" onClick={submitLogin}>Acessar</button>
+        </div>
     )
 }
