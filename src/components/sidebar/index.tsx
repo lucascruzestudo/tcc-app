a8f347180c1cd8f5f07ce3247662ee6081996729
Allyson-Santana <santana.allyson459@gmail.com>
2024-04-01 15:03:39 -0300

    feat: user infor in sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,8 +3,12 @@ import "./index.css"
 import { Link } from "react-router-dom";
 import { menus } from "./menus";
 import AuthService from "src/services/auth";
+import { useAuth } from "src/hooks/authContextProvider";
+import { UserRole } from "@utils/enums";
+import defaultProfile from "@assets/profile.jpeg";
 
 export function SideBar() {
+    const userLocalStorage = useAuth().user!
     const authService = new AuthService();
 
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -21,6 +25,12 @@ export function SideBar() {
         localStorage.removeItem('refreshToken');
 
         window.location.href = '/';
+    }
+
+    const getProfileName = (role: UserRole) => {
+        if (role === UserRole.COORDINATOR) return "Coordenador";
+        else if (role === UserRole.ADVISOR) return "Orientador";
+        else if (role === UserRole.STUDENT) return "Aluno";
     }
 
     return (
@@ -50,10 +60,10 @@ export function SideBar() {
 
                 <li className="profile">
                     <div className="profile_details">
-                        <img src="/src/assets/rui-iconica-profile.jpeg" alt="profile image" />
+                        <img src={userLocalStorage.profile_picture || defaultProfile} alt="profile image" />
                         <div className="profile_content">
-                            <div className="name">Rui Silvestrin</div>
-                            <div className="designation">Orientador</div>
+                            <div className="name">{userLocalStorage.full_name}</div>
+                            <div className="designation">{getProfileName(userLocalStorage.role)}</div>
                         </div>
                     </div>
                     <i className="bx bx-log-out" id="log_out" onClick={handleLogout}></i>
